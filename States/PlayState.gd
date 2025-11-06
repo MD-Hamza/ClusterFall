a439054e1906d3a9f4850bf2fe969d10c9d17de0
MD-Hamza <71731160+MD-Hamza@users.noreply.github.com>
2025-11-05 23:53:41 -0500

    Obstacle collision detection

--- States/PlayState.gd
+++ States/PlayState.gd
@@ -4,7 +4,11 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Spawner.destroyed.connect(_teleport_player);
+	$Obstacle.area_entered.connect(_on_obstacle_collide)
 
+func _on_obstacle_collide(area: Area2D):
+	print("Entered by:", area.name)
+	
 func _teleport_player(position: Vector2):
 	$Player.position = position
 	
