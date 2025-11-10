e426b540f381524b198c9b73a9b5a7e80d3fd805
MD-Hamza <71731160+MD-Hamza@users.noreply.github.com>
2025-11-09 23:55:18 -0500

    Connected collision event to obstacles

--- Obstacle/Obstacle.gd
+++ Obstacle/Obstacle.gd
@@ -9,3 +9,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_body_entered(body):
+	if (body.name == "Player"):
+		get_tree().call_group("playstate", "game_over")
--- States/PlayState.gd
+++ States/PlayState.gd
@@ -4,13 +4,13 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Spawner.destroyed.connect(_teleport_player);
-	$Obstacle.area_entered.connect(_on_obstacle_collide)
 
-func _on_obstacle_collide(area: Area2D):
-	print("Entered by:", area.name)
 	
 func _teleport_player(position: Vector2):
 	$Player.position = position
+
+func game_over():
+	$Player.position = Vector2.ZERO
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
